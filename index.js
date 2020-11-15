b4000f73ccfcea66c5829c519ac405e3831dd719
Radhe Pokhrel <rshyampokhrel@gmail.com>
2020-11-14 23:36:56 -0500

    Refactored the layout of the index.js file

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-require('dotenv').config();
+const INFO = require('./info.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
-
-bot.login(TOKEN);
+const TOKEN = INFO["BOT_TOKEN"];
 
 bot.on('ready', () => {
+  var d = new Date();
+  var n = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
   console.info(`Logged in as ${bot.user.tag}!`);
+  console.log(`Time : ${n}`);
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
-  }
+  if (msg.author == bot.user){
+    return
+  } else {
+    processCommand(msg);
+  } 
 });
+
+function processCommand(receivedMessage) {
+  let fullCommand = receivedMessage.content.substr(1)
+  let splitCommand = fullCommand.split(" ");
+  let primeCom = splitCommand[0];
+  if (primeCom == 'ping') {
+    ping(receivedMessage);
+  } else {
+    receivedMessage.reply('I did not understand what you said...');
+  }
+};
+function ping(message) {
+  if (message.content == '!ping') {
+    message.reply('Pong');
+    var now = new Date();
+    var timeRn = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    console.log(`Ping responded to at ${timeRn}`);
+  } else {
+    console.log('Message not understood')
+  }
+};
+
+bot.login(TOKEN);
