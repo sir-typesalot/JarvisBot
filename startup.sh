4f25940ffb74124c84ed20e4d6723e3023bea8d2
Radhe Pokhrel <rshyampokhrel@gmail.com>
2022-05-13 08:46:28 -0400

    Give up, going nohup

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-echo "Starting up app at $(date)"
-name="app_screen"
-echo "Compiling new executable"
 cd ~/JarvisBot/
+echo "Compiling new executable"
 go build -o bin
-screen -c log.conf -dm -S $name bin/jarvis-bot
-echo "Restarted detached screen $name"
+nohup bin/jarvis-bot > logfile.txt &
