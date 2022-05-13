8559d4cdac3ff6aa5de924fbcb7e741d3e4b5942
Radhe Pokhrel <rshyampokhrel@gmail.com>
2022-05-13 07:29:17 -0400

    Changes to startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 echo "Last reboot time: $(date)" > /etc/motd
-screen -dm -S "app_dev" ./jarvis-bot
-echo "Restarted detached screen 'app_dev'"
+name="app_dev"
+path="/var/log";
+config="logfile ${path}/${name}.log
+logfile flush 1
+log on
+logtstamp after 1
+logtstamp string \"[ %t: %Y-%m-%d %c:%s ]\012\"
+logtstamp on";
+echo "$config" > /tmp/log.conf
+echo "Created log file"
+screen -c /tmp/log.conf -dmSL "$name" ./jarvis-bot
+rm /tmp/log.conf
+echo "Restarted detached screen $name"
