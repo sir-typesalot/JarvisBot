48a1f56e10a03ef147ecf7bea56823f5a6d8c720
Radhe Pokhrel <rshyampokhrel@gmail.com>
2022-05-13 08:35:02 -0400

    More script work

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 echo "Starting up app at $(date)"
 name="app_screen"
 echo "Compiling new executable"
+ls ~/JarvisBot/
 go build -o bin
 screen -c log.conf -dm -S $name bin/jarvis-bot
 echo "Restarted detached screen $name"
