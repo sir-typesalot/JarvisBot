7786be128701a68db77e702f651f85e9dda222cb
Radhe Pokhrel <rshyampokhrel@gmail.com>
2022-05-13 11:00:54 -0400

    Extra to scripts

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 cd ~/JarvisBot/
+echo "Checking for bin folder"
+if [ ! -d "./bin" ]
+then
+    echo "bin folder not found... Creating..." 
+    mkdir bin
+fi
 echo "Compiling new executable"
 go build -o bin
+echo "Starting nohup operation in background"
+echo "Logs are stored in logfile.txt"
 nohup ./bin/jarvis-bot > logfile.txt &
